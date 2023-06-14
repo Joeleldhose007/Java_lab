db73560604d33cdcc3443db609370e5983faaa5e
Joel Eldhose <117965108+Joeleldhose007@users.noreply.github.com>
2023-06-14 13:43:57 +0530

    Add files via upload

---
+++ value/src/value/Value.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package value;
+
+/**
+ *
+ * @author joel
+ */
+public class Value {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        classs ob =new classs();
+        ob.setx(200);
+        System.out.print(ob.getx());
+    }
+    
+}
